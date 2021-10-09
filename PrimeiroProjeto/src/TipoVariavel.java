74b91703584ebdd28813892199df9a5803e30add
Paulo Queiroz <queiroz1987@hotmail.com>
2021-10-09 14:00:27 -0300

    Aula_Sabado_0910

---
+++ PrimeiroProjeto/src/TipoVariavel.java
@@ -0,0 +1,26 @@
+
+public class TipoVariavel {
+
+	public static void main(String[] args) {
+		
+		byte tipoByte = 123;
+		short tipoShort = 12345;
+		char tipoChar = 'a';
+		float tipoFloat = 2.1f;
+		double tipoDouble = 1.52;
+		int tipoInt = 1234567;
+		long tipoLong = 1234567890l;
+		boolean tipoBoolean = true;
+		
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoChar = "  + tipoChar);
+		System.out.println("Valor do tipoFloat = " + tipoFloat);
+		System.out.println("Valor do tipoDouble = " + tipoDouble);
+		System.out.println("Valor do tipoInt = " + tipoInt);
+		System.out.println("Valor do tipoLong = " + tipoLong);
+		System.out.println("Valor do tipoBooleano = " + tipoBoolean);
+
+	}
+
+}
