8576440fe4d2fa01e156455d7a19e228c615854f
Paulo Queiroz <queiroz1987@hotmail.com>
2021-10-17 17:25:22 -0300

    Exercícios da aula do dia 15out21
    Exercícios de reforço utilizando condicionais e repetição

---
+++ Aula_15_10/src/Exerc2_1510.java
@@ -0,0 +1,21 @@
+
+public class Exerc2_1510 {
+
+	/*2- Faça um programa que mostre uma contagem na tela 
+	 * de 233 a 456, só que contando de 3 em 3 quando estiver 
+	 * entre 300 e 400 e de 5 em 5 quando não estiver.*/
+	
+	public static void main(String[] args) {
+		
+		for(int i = 233; i>=233 && i<=456; i+=5) {
+			System.out.println(i);
+			if(i>=300 && i<=400) i -= 2;
+		}
+		
+		/*for (int i=233; i<=456 ; i +=(i>=300 && i<=400) ? 3 : 5) {
+			System.out.print(i + ",");
+		}*/
+	
+	}
+
+}
